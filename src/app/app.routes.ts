ac2783fc960677fb3bdd049de610c927d12926c0
MatSant0s <santosmateo.2002@gmail.com>
2026-07-14 13:34:33 -0500

    feat(pokemon-ssr): configuration of initial routes navegation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about-pages/about-pages'),
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./pages/contact-page/contact-page'),
+  },
+  {
+    path: 'pricing',
+    loadComponent: () => import('./pages/pricing-pages/pricing-pages'),
+  },
+
+  {
+    path: '**',
+    redirectTo: 'about',
+  },
+];
---
+++ src/app/pages/about-pages/about-pages.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './about-pages.html',
+})
+export default class AboutPages {}
