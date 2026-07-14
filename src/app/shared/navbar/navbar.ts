78347bd3c2c74ede889b653107b6bf1bc2c3a4b5
MatSant0s <santosmateo.2002@gmail.com>
2026-07-14 15:53:49 -0500

    feat(pokemon-ssr): navbar without selectors component

---
+++ src/app/shared/navbar/navbar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink],
+  templateUrl: './navbar.html',
+})
+export class Navbar {}
